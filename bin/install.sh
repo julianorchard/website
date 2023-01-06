bdb58bcb8880aa3820c529a753c73c45290ff92f
Julian Orchard <hello@julianorchard.co.uk>
2023-01-06 22:54:58 +0000

    fix: issues with cygwin install

--- bin/install.sh
+++ bin/install.sh
@@ -1,37 +1,20 @@
-#!/bin/sh
+#!/bin/bash
 
 ## install .sh  ---  Slightly edited from the original which is (c) Nicola Paolucci.
 
 ## Description:
 
-# Slightly edited by me to install/manage my dotfiles respository.
+# Written by Nicola Paolucci, edited by me to install/manage my 
+# dotfiles respository.
 
 # Original source:
 # https://bitbucket.org/durdn/cfg/src/master/.bin/install.sh
 
-## License: 
-
-# The MIT License (MIT)
+## License:
 
 # Copyright (c) 2018 Nicola Paolucci
 
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
+# Copyright (c) 2023 Julian Orchard <jorchard@pm.me>
 
 ## Code:
 
@@ -47,8 +30,10 @@ config checkout
 if [ $? = 0 ]; then
   echo "Checked out config.";
 else
-  echo "Backing up pre-existing dot files.";
-  config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .dotfiles-backup/{}
+  echo "Backing up pre-existing dotfiles.";
+
+  # TODO: Replace grep with sed only?
+  config checkout 2>&1 | grep -E "^\s+.*$" | sed 's/\s+//g' | xargs -I{} sh -c 'mkdir -p `echo $(dirname "${HOME}/.dotfiles-backup/{}")` && mv "${HOME}/{}" "${HOME}/.dotfiles-backup/{}"'
 fi
 
 config checkout
